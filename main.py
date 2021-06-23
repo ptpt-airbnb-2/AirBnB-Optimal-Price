b3a800453fa4f30e0164c8b2481a3a501dd76300
Jacob Torres <jacob@jacobtorres.net>
2021-06-23 11:54:06 -0600

    Added a dollar sign to prediction output.

--- main.py
+++ main.py
@@ -6,18 +6,12 @@ from flask import Flask, render_template, redirect, request
 from tensorflow.keras.models import load_model
 from pathlib import Path
 
-
-
 BASE_DIR = Path(__file__).parent.parent
-
-
-
-
 app = Flask(__name__)
+
 # Import model
-
-
 model = load_model(filepath="airbnbpredict_all_data.h5")
+
 # Define the column names for importing the user data
 cols = ['latitude', 'longitude', 'neighbourhood', 'room_type',
         'minimum_nights', 'number_of_reviews',
@@ -32,24 +26,23 @@ async def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    """Get the information from the home.html and returns the predicted value."""
+    """Gets the information from the home.html and returns the predicted value."""
+
     # Collect the user data and change to array and then to DataFrame
     user_data = [float(x) for x in request.form.values()]
     user_data_np = np.array(user_data)
     user_data_df = pd.DataFrame([user_data_np], columns=[cols])
+
     # Use model to make prediction
     prediction = model.predict(user_data_df)
 
-    if prediction < 0:
+    if prediction < 0 or prediction > 2000000:
         return render_template('home.html',
-                               pred='Your set parameters are out of range something trained model has not seen!')
+                               pred='Your set parameters are out of range!')
 
-    elif prediction > 2000000:
-        return render_template('home.html',
-                               pred='Your set parameters are out of range something! Please revisit them.')
     else:
         return render_template('home.html',
-                               pred='Your property should be listed at {} in the '
+                               pred='Your property should be listed at ${} in the '
                                     'local currency of the geo '
                                     'location.'.format(int(prediction[0][0])))
 
